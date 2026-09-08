6a70a0862ae929673e68f09bc4926951347b6dbb
9182Aishwarya <9182aishwarya@gmail.com>
2026-09-08 09:55:21 +0530

    Update Process_student.c

--- src/Process_student.c
+++ src/Process_student.c
@@ -3,51 +3,41 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
-
 #include "student.h"
-
 extern int count;
-
 struct Student
 {
-    int id;
+int id;
     char name[30];
 };
-
 extern struct Student s[];
-
 void createProcesses()
 {
     int i;
 
-    if(count == 0)
+    if(count==0)
     {
         printf("No students available\n");
         return;
     }
 
-    printf("Parent PID = %d\n\n", getpid());
+    printf("Parent PID=%d\n\n",getpid());
 
-    for(i = 0; i < count; i++)
+    for(i = 0;i < count;i++)
     {
         pid_t pid = fork();
-
         if(pid == 0)
         {
             printf("Student ID   : %d\n", s[i].id);
             printf("Student Name : %s\n", s[i].name);
             printf("PID          : %d\n", getpid());
             printf("PPID         : %d\n", getppid());
-            printf("-------------------\n");
-
             exit(0);
         }
     }
-
     for(i = 0; i < count; i++)
     {
         wait(NULL);
     }
-
     printf("All child processes completed\n");
 }
